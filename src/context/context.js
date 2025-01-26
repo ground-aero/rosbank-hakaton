d1be9da5bb7d174b87139a14c74763123bd279cd
unknown <ea.avdeev@gmail.com>
2025-01-26 23:28:32 +0300

    delete context.js

--- src/context/context.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// TeamContext
-export const TeamContext = React.createContext({
-    isEmployeeId: null,
-    isTeamId: null,
-    isTeamTotal: 0,
-    isBusFactor: 0,
-});
